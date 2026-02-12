85337effcfc8c17c8750e031a76eb956eb9add5c
Karthik200619 <23eg112e53@anurag.edu.in>
2026-02-12 20:01:50 +0530

    Update userAPI.js

--- week05/APIs/userAPI.js
+++ week05/APIs/userAPI.js
@@ -2,7 +2,8 @@ import exp from 'express'
 import { ArticleModel } from '../models/ArticleModel.js'
 import { register,authenticate } from '../services/authService.js'
 import { verifyToken } from '../middlewares/validateToken.js'
-import { checkUser } from '../middlewares/checkUser.js'
+import { UserTypeModel } from '../models/UserModel.js'
+
 
 export const userRoute = exp.Router()
 
@@ -18,15 +19,9 @@ userRoute.post('/users', async(req, res) => {
     res.status(201).json({ message: "user registered successfully" })
 })
 
-// Autheticate user
-// userRoute.post("/authenticate",async(req,res)=>{
-    
-// })
-
-
 
 // read all articles
-userRoute.get('/articles/:userId',verifyToken,checkUser,async(req,res)=>{
+userRoute.get('/articles/:userId',verifyToken,async(req,res)=>{
     //check he is a user or not
     // get all articles from ArticleModel
     let articles=await ArticleModel.find()
@@ -34,3 +29,26 @@ userRoute.get('/articles/:userId',verifyToken,checkUser,async(req,res)=>{
 })
 
 // Add comment to an article
+userRoute.put('/articles/:articleId/comments/users/:userId',verifyToken,async(req,res)=>{
+    //get comment from body
+    let {commentTxt}=req.body
+    //get userId and artcileId
+    let {userId,articleId}=req.params
+    //check weather they both are vaild or not
+    //get userObj from Db
+    let userObjDb=await UserTypeModel.findById(userId)
+    //get articleObj from Db
+    let articleObjDb=await ArticleModel.findById(articleId)
+    //check userObj is valid
+    if(!userObjDb){
+        return res.status(401).json({message:"user not found"})
+    }
+    //check articleObj is valid
+    if(!articleObjDb){
+        return res.status(401).json({message:"user not found"})
+    }
+    //if all vaild add comment by getting aryicleId and update
+    let addedComment=await ArticleModel.findByIdAndUpdate(articleId,{$set:{comments:{user:userId,comment:commentTxt}}},{new:true})
+    //return response
+    res.status(200).json({message:"comment added successfully",payload:addedComment})
+})
